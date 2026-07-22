776bb67c1a48a4cdb9ead0e7a6e0d1e381d5e524
islam çelik <147990237+islamcelik0720@users.noreply.github.com>
2026-07-22 15:22:32 +0300

    Integrated Firebase Authentication setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'screens/auth/login_screen.dart';
-import 'core/theme/app_theme.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'core/theme/app_theme.dart';
+import 'screens/auth/login_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const WardrobeAIApp());
 }
 
@@ -13,6 +20,7 @@ class WardrobeAIApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'WardrobeAI',
       theme: AppTheme.lightTheme,
       home: const LoginScreen(),
     );
